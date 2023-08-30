8e74b5103f2103ebc172f688b8f47b25447eed9c
FelipeArredond <pipechiqui77@gmail.com>
2023-08-30 10:06:25 -0500

    Find by id controller created

--- src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
+++ src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
@@ -5,6 +5,7 @@ import com.bolsadeideas.springboot.webflux.app.models.services.ProductoService;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -26,5 +27,12 @@ public class ProductoController {
                         .contentType(MediaType.APPLICATION_JSON)
                         .body(this.productoService.findAll())
         );
+    }
+
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<Producto>> obtenerUno(@PathVariable String id) {
+        return this.productoService.findById(id)
+                .map(p -> ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(p))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 }
